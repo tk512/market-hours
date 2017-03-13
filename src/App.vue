73b0640f38c8e37de1aa3f112d6ab27094ab4b3c
Torbjørn Kristoffersen <torbjoern@gmail.com>
2017-03-13 19:46:05 +0200

    Switched back to hash router mode

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,12 @@
               aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="/">
-        <i class="fa fa-line-chart" aria-hidden="true"></i> Market Hours
-      </a>
+      <a class="navbar-brand" href="#/">
+        <i class="fa fa-line-chart" aria-hidden="true"></i> Market Hours</a>
       <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item" v-bind:class="{'active': $route.path==='/'}">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#/">Home <span class="sr-only">(current)</span></a>
           </li>
           <!--
           <li class="nav-item">
@@ -20,7 +19,7 @@
           </li>
           -->
           <li class="nav-item" v-bind:class="{'active': $route.path==='/about'}">
-            <a class="nav-link" href="/about">About</a>
+            <a class="nav-link" href="#/about">About</a>
           </li>
         </ul>
         <span class="badge badge-pill badge-default test" id="your-time">
